dd8c5428194305c845f168f25e27c234789e3984
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-13 13:59:52 -0300

    Fim das alteracoes de layout das páginas individuais de cada cartunista.

--- cecilia.php
+++ cecilia.php
@@ -1,45 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
-<?php require "includes/head.php";?>
+<?php require "includes/head.php"; ?>
 
 <title>Cecilia Martins</title>
 
-  <body>
-    <div class="container text-center">
-<!------------------------------------------------- Chamando Header ------------------------------------------------->
-      <?php require "includes/header_nav.php";?>
-      <article>
-        <figure>
-            <img src="https://placehold.co/1000x100/000000/FFF" alt="">
-        </figure>
+<body>
+  <div class="container text-center">
+    <!------------------------------------------------- Chamando Header ------------------------------------------------->
+    <?php require "includes/header_nav.php"; ?>
+    <article>
 
-        <h1>Cecilia Martins</h1>
-        <a href="https://www.instagram.com/ceciliatangerina?igsh=MWJ4emVocm04bzE2NA=="><b>@ceciliatangerina</b></a>
+      <hr>
+      <figure>
+        <img src="https://placehold.co/1000x100/000000/FFF" alt="">
+      </figure>
+      <hr>
 
-        <figure>
+      <h1>Cecilia Martins</h1>
+      <a href="https://www.instagram.com/ceciliatangerina?igsh=MWJ4emVocm04bzE2NA==" target="_blank"><b>@ceciliatangerina</b></a>
+      <hr>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <figure>
             <img src="https://placehold.co/500x500/000000/FFF" alt="">
-        </figure>
+          </figure>
+        </div>
 
-        <p>A mente criativa por trás da impactante Reportagem em HQ <strong>“Parque das Luzes”</strong>, esta artista talentosa narra as vivências de mulheres em situação de prostituição no icônico Parque das Luzes, o mais antigo de São Paulo. Sua habilidade como ilustradora também brilhou no documentário <strong>AmarElo”</strong>, uma produção do Emicida para a Netflix, que recebeu uma merecida <strong>indicação ao Emmy Internacional.</strong></p>
-        <p>
-        Como parte de uma equipe internacional, ela desempenhou um papel fundamental em levar o jornalismo em quadrinhos brasileiro além-fronteiras com a reportagem <strong>“Favela vs. COVID-19”</strong>. Esta colaboração com o portal polonês Outride foi reconhecida com o prêmio de melhor reportagem de saúde pelo prestigioso International Center For Journalists.
-        </p>
-        <p>
-        Explorando temas complexos, sentimentos profundos e fatos surpreendentes, ela tem um gosto eclético que abrange desde o rap até quadrinhos com temáticas densas. E, claro, não podemos esquecer de sua paixão por comida apimentada, um detalhe picante que adiciona ainda mais sabor à sua personalidade vibrante.
-        </p>
-        <figure>
-            <img src="https://placehold.co/1200x300/000000/FFF" alt="">
-        </figure>
-      </article>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <p>A mente criativa por trás da impactante Reportagem em HQ <strong>“Parque das Luzes”</strong>, esta artista talentosa narra as vivências de mulheres em situação de prostituição no icônico Parque das Luzes, o mais antigo de São Paulo. Sua habilidade como ilustradora também brilhou no documentário <strong>AmarElo”</strong>, uma produção do Emicida para a Netflix, que recebeu uma merecida <strong>indicação ao Emmy Internacional.</strong></p>
+          <p>
+            Como parte de uma equipe internacional, ela desempenhou um papel fundamental em levar o jornalismo em quadrinhos brasileiro além-fronteiras com a reportagem <strong>“Favela vs. COVID-19”</strong>. Esta colaboração com o portal polonês Outride foi reconhecida com o prêmio de melhor reportagem de saúde pelo prestigioso International Center For Journalists.
+          </p>
+          <p>
+            Explorando temas complexos, sentimentos profundos e fatos surpreendentes, ela tem um gosto eclético que abrange desde o rap até quadrinhos com temáticas densas. E, claro, não podemos esquecer de sua paixão por comida apimentada, um detalhe picante que adiciona ainda mais sabor à sua personalidade vibrante.
+          </p>
+        </div>
+      </div>
 
-<!------------------------------------------------- Chamando Footer ------------------------------------------------->
-      <?php require "includes/footer.php";?>
+      <hr>
+      <figure>
+        <img src="https://placehold.co/1200x300/000000/FFF" alt="">
+      </figure>
+      <hr>
 
-<!------------------------------------------------- Chamando Java ------------------------------------------------->
-      <?php require "includes/java.php";?>
+    </article>
 
-    </div>
-  </body>
+    <!------------------------------------------------- Chamando Footer ------------------------------------------------->
+    <?php require "includes/footer.php"; ?>
+
+    <!------------------------------------------------- Chamando Java ------------------------------------------------->
+    <?php require "includes/java.php"; ?>
+
+  </div>
+</body>
 
 </html>
--- guilherme.php
+++ guilherme.php
@@ -1,52 +1,65 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
-<?php require "includes/head.php";?>
+<?php require "includes/head.php"; ?>
 
 <title>Guilherme Infante</title>
 
-  <body>
-    <div class="container text-center">
-<!------------------------------------------------- Chamando Header ------------------------------------------------->
-      <?php require "includes/header_nav.php";?>
-      <article>
-        <figure>
-            <img src="https://placehold.co/1000x100/000000/FFF" alt="">
-        </figure>
+<body>
+  <div class="container text-center">
+    <!------------------------------------------------- Chamando Header ------------------------------------------------->
+    <?php require "includes/header_nav.php"; ?>
+    <article>
+      <hr>
+      <figure>
+        <img src="https://placehold.co/1000x100/000000/FFF" alt="">
+      </figure>
+      <hr>
 
-        <h1>Guilherme Infante</h1>
-        <a href=""><b>@o_capirotinho</b></a>
-        <br>
-        <a href="https://www.instagram.com/guilherme.infante?igsh=MXNxazk0Zm14cGY0aQ=="><b>@guilherme.infante</b></a>
+      <h1>Guilherme Infante</h1>
+      <a href="https://www.instagram.com/o_capirotinho/" target="_blank"><b>@o_capirotinho</b></a>
+      <br>
+      <a href="https://www.instagram.com/guilherme.infante?igsh=MXNxazk0Zm14cGY0aQ==" target="_blank"><b>@guilherme.infante</b></a>
+      <hr>
 
-        <figure>
+      <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <figure>
             <img src="https://placehold.co/500x500/000000/FFF" alt="">
-        </figure>
+          </figure>
+        </div>
 
-        <p>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <p>
             Renomado quadrinista brasileiro que, em 2014, deu vida ao personagem <strong>Capirotinho</strong>, um pequeno diabo repleto de sarcasmo que oferece conselhos perspicazes sobre uma variedade de temas cotidianos. As tiras e cartuns protagonizados por esse personagem são compartilhados diariamente em sua fanpage no Facebook, que viu um salto impressionante de 16 mil fãs em 2014 para 115 mil em 2017.
-        </p>
-        <p>
+          </p>
+          <p>
             Em 2017, Guilherme deu um grande passo ao lançar a primeira coletânea impressa, intitulada “O Capirotinho: uma Dose de Porquês Antes do Fim”, contendo 192 páginas e 150 charges inéditas. No ano seguinte, veio a segunda coletânea de tiras, novamente pela editora Lendari em seu recém-criado selo de quadrinhos PULP.
-        </p>
-        <p>
+          </p>
+          <p>
             Em 2018, Infante expandiu ainda mais seu repertório literário com o lançamento de três novos livros: “O Manual para os Dias Cinzentos”, “As Cicatrizes do Circo Sabiá” (com arte de Laz Muniz) e “Antologias Poéticas de Outono”, uma coleção que combina poesia e ilustrações de forma harmoniosa.
-        </p>
-        <p>
-        <strong>Em 2021, Infante ganhou o Prêmio Angelo Agostini de melhor web quadrinho</strong> por seu trabalho nas tiras do Capirotinho, consolidando ainda mais seu lugar no mundo dos quadrinhos brasileiros.  
-        </p>
-        <figure>
-            <img src="https://placehold.co/1200x300/000000/FFF" alt="">
-        </figure>
-      </article>
+          </p>
+          <p>
+            <strong>Em 2021, Infante ganhou o Prêmio Angelo Agostini de melhor web quadrinho</strong> por seu trabalho nas tiras do Capirotinho, consolidando ainda mais seu lugar no mundo dos quadrinhos brasileiros.
+          </p>
+        </div>
+      </div>
 
-<!------------------------------------------------- Chamando Footer ------------------------------------------------->
-      <?php require "includes/footer.php";?>
+      <hr>
+      <figure>
+        <img src="https://placehold.co/1200x300/000000/FFF" alt="">
+      </figure>
+      <hr>
 
-<!------------------------------------------------- Chamando Java ------------------------------------------------->
-      <?php require "includes/java.php";?>
+    </article>
 
-    </div>
-  </body>
+    <!------------------------------------------------- Chamando Footer ------------------------------------------------->
+    <?php require "includes/footer.php"; ?>
+
+    <!------------------------------------------------- Chamando Java ------------------------------------------------->
+    <?php require "includes/java.php"; ?>
+
+  </div>
+</body>
 
 </html>

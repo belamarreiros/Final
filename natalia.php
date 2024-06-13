f31c1d59fc8e1a11231ae6bee7344491afaecc4e
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-13 12:46:34 -0300

    Alteracao rapha.php e nathalia.php. Apenas deixando dentro dos layouts legais.

--- natalia.php
+++ natalia.php
@@ -9,30 +9,44 @@
   <div class="container text-center">
     <!------------------------------------------------- Chamando Header ------------------------------------------------->
     <?php require "includes/header_nav.php"; ?>
+
     <article>
-      <figure>
+      <hr>
+      <figure class="mt-3">
         <img src="https://placehold.co/1000x100/000000/FFF" alt="">
       </figure>
+      <hr>
 
       <h1>Natalia Serpinski</h1>
-      <a href="https://www.instagram.com/n.sierpinski?igsh=Zm1rY211Z2h6OGIy"><b>@n.sierpinski</b></a>
+      <a href="https://www.instagram.com/n.sierpinski?igsh=Zm1rY211Z2h6OGIy" target="_blank"><b>@n.sierpinski</b></a>
+      <hr>
 
-      <figure>
-        <img src="https://placehold.co/500x500/000000/FFF" alt="">
-      </figure>
+      <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <figure>
+            <img src="https://placehold.co/500x500/000000/FFF" alt="">
+          </figure>
+        </div>
 
-      <p>
-        Com um mestrado em <strong>Ciências da Comunicação pela renomada USP</strong>, mergulhou profundamente no universo das histórias em quadrinhos, com um foco especial na autoria feminina. Além disso, é licenciada em Educomunicação, onde desenvolveu um projeto inovador que utiliza as HQs como ferramenta para discutir questões de gênero nas escolas.
-      </p>
-      <p>
-        Desde 2014, tem dedicado sua carreira acadêmica à exploração da relação entre gênero e histórias em quadrinhos. Suas descobertas e perspectivas foram compartilhadas em artigos para os sites <strong>Garotas Nerds e Minas Nerds</strong>, contribuindo para a discussão sobre o assunto.
-      </p>
-      <p>
-        Atualmente, tem a honra de coordenar a programação do <strong>Butantã Gibi Con</strong>, um evento premiado que celebra a rica cultura das HQs. Este papel á permite continuar a promover discussões importantes sobre diversidade e representatividade nos quadrinhos, contribuindo para um futuro mais inclusivo e diversificado no mundo das HQs.
-      </p>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <p>
+            Com um mestrado em <strong>Ciências da Comunicação pela renomada USP</strong>, mergulhou profundamente no universo das histórias em quadrinhos, com um foco especial na autoria feminina. Além disso, é licenciada em Educomunicação, onde desenvolveu um projeto inovador que utiliza as HQs como ferramenta para discutir questões de gênero nas escolas.
+          </p>
+          <p>
+            Desde 2014, tem dedicado sua carreira acadêmica à exploração da relação entre gênero e histórias em quadrinhos. Suas descobertas e perspectivas foram compartilhadas em artigos para os sites <strong>Garotas Nerds e Minas Nerds</strong>, contribuindo para a discussão sobre o assunto.
+          </p>
+          <p>
+            Atualmente, tem a honra de coordenar a programação do <strong>Butantã Gibi Con</strong>, um evento premiado que celebra a rica cultura das HQs. Este papel á permite continuar a promover discussões importantes sobre diversidade e representatividade nos quadrinhos, contribuindo para um futuro mais inclusivo e diversificado no mundo das HQs.
+          </p>
+        </div>
+      </div>
+
+      <hr>
       <figure>
         <img src="https://placehold.co/1200x300/000000/FFF" alt="">
       </figure>
+      <hr>
+
     </article>
 
     <!------------------------------------------------- Chamando Footer ------------------------------------------------->
--- rapha.php
+++ rapha.php
@@ -1,54 +1,66 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
-<?php require "includes/head.php";?>
+<?php require "includes/head.php"; ?>
 
 <title>Rapha Pinheiro</title>
 
-  <body>
-    <div class="container text-center">
-<!------------------------------------------------- Chamando Header ------------------------------------------------->
-      <?php require "includes/header_nav.php";?>
-      <article>
-        <figure>
-            <img src="https://placehold.co/1000x100/000000/FFF" alt="">
-        </figure>
+<body>
+  <div class="container text-center">
+    <!------------------------------------------------- Chamando Header ------------------------------------------------->
+    <?php require "includes/header_nav.php"; ?>
 
-        <h1>Rapha Pinheiro</h1>
-        <a href=" https://www.instagram.com/raphacpinheiro?igsh=eGEzOTJpNDk2Y2xy"><b>@raphacpinheiro</b></a>
-       
-        <figure>
+    <article>
+      <hr>
+      <figure>
+        <img src="https://placehold.co/1000x100/000000/FFF" alt="">
+      </figure>
+      <hr>
+
+      <h1>Rapha Pinheiro</h1>
+      <a href=" https://www.instagram.com/raphacpinheiro?igsh=eGEzOTJpNDk2Y2xy" target="_blank"><b>@raphacpinheiro</b></a>
+      <hr>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <figure class="mt-3">
             <img src="https://placehold.co/500x500/000000/FFF" alt="">
-        </figure>
+          </figure>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <p>
+            Com uma formação diversificada e internacional, sua jornada acadêmica o levou da <strong>FAU UFRJ no Brasil à University of Lincoln na Inglaterra, e até mesmo à École Européenne Superieure de l’Image em Angoulême, na França</strong>. Hoje, é um orgulhoso mestre em Mídias Criativas pela ECO UFRJ.
+          </p>
+          <p>
+            Atualmente, está à frente da Diretoria Executiva de Arte e Cultura na Fundação Cultural Jornalista Oswaldo Lima em Campos dos Goytacazes. Também lidera o Inko - Escola de Mídias Criativas, onde moldam a próxima geração de artistas para os mercados nacional e internacional.
+          </p>
+          <p>
+            Como Editor-Chefe do Universo Guará, tem a honra de supervisionar a produção e publicação de diversos artistas talentosos e títulos em nível nacional. Além disso, é o criador do <strong>Funktoon, o maior aplicativo de leitura de quadrinhos brasileiros</strong> do país, com mais de 2 milhões de leituras.
+          </p>
+          <p>
+            Sua paixão pela escrita o levou a criar obras aclamadas como “Os Tomos de Tessa”, “Salto”, “Silas”, “Mesa 44”, “Travessia e Ecos”, “Rua”, “O Mago e a Tempestade”, além de diversos projetos menores e colaborativos. Também é responsável pela organização da coleção Inko, que reúne trabalhos dos veteranos da escola.
+          </p>
+          <p>
+            Seu trabalho já foi reconhecido com prêmios prestigiosos como o <Strong>roféu HQMix</Strong>, o <strong>Cubo de Ouro</strong> e o <Strong>Prêmio do Dia do Super-Herói Brasileiro</Strong>. Além disso, teve o privilégio de expor sua arte em locais tão diversos quanto a França, o Rio de Janeiro, Santos, Manaus e Campos
+          </p>
+        </div>
+      </div>
+      <hr>
 
-        <p>
-          Com uma formação diversificada e internacional, sua jornada acadêmica o levou da <strong>FAU UFRJ no Brasil à University of Lincoln na Inglaterra, e até mesmo à École Européenne Superieure de l’Image em Angoulême, na França</strong>. Hoje, é um orgulhoso mestre em Mídias Criativas pela ECO UFRJ.
-        </p>
-        <p>
-          Atualmente, está à frente da Diretoria Executiva de Arte e Cultura na Fundação Cultural Jornalista Oswaldo Lima em Campos dos Goytacazes. Também lidera o Inko - Escola de Mídias Criativas, onde moldam a próxima geração de artistas para os mercados nacional e internacional.
-        </p>
-        <p>
-          Como Editor-Chefe do Universo Guará, tem a honra de supervisionar a produção e publicação de diversos artistas talentosos e títulos em nível nacional. Além disso, é o criador do <strong>Funktoon, o maior aplicativo de leitura de quadrinhos brasileiros</strong> do país, com mais de 2 milhões de leituras.
-        </p>
-        <p>
-          Sua paixão pela escrita o levou a criar obras aclamadas como “Os Tomos de Tessa”, “Salto”, “Silas”, “Mesa 44”, “Travessia e Ecos”, “Rua”, “O Mago e a Tempestade”, além de diversos projetos menores e colaborativos. Também é responsável pela organização da coleção Inko, que reúne trabalhos dos veteranos da escola.
-        </p>
-        <p>
-          Seu trabalho já foi reconhecido com prêmios prestigiosos como o <Strong>roféu HQMix</Strong>, o <strong>Cubo de Ouro</strong> e o <Strong>Prêmio do Dia do Super-Herói Brasileiro</Strong>. Além disso, teve o privilégio de expor sua arte em locais tão diversos quanto a França, o Rio de Janeiro, Santos, Manaus e Campos
-        </p>
+      <figure>
+        <img src="https://placehold.co/1200x300/000000/FFF" alt="">
+      </figure>
+      <hr>
 
-        <figure>
-            <img src="https://placehold.co/1200x300/000000/FFF" alt="">
-        </figure>
-      </article>
+    </article>
 
-<!------------------------------------------------- Chamando Footer ------------------------------------------------->
-      <?php require "includes/footer.php";?>
+    <!------------------------------------------------- Chamando Footer ------------------------------------------------->
+    <?php require "includes/footer.php"; ?>
 
-<!------------------------------------------------- Chamando Java ------------------------------------------------->
-      <?php require "includes/java.php";?>
+    <!------------------------------------------------- Chamando Java ------------------------------------------------->
+    <?php require "includes/java.php"; ?>
 
-    </div>
-  </body>
+  </div>
+</body>
 
 </html>

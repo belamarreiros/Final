d2b5e531e30e2c30598c584e03f8f098be423ad6
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-13 13:47:14 -0300

    Alteracoes nas páginas de alguns cartunistas. Ainda faltam algumas.

--- daniel.php
+++ daniel.php
@@ -1,48 +1,60 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
-<?php require "includes/head.php";?>
+<?php require "includes/head.php"; ?>
 
 <title>Daniel Esteves</title>
 
-  <body>
+<body>
     <div class="container text-center">
-<!------------------------------------------------- Chamando Header ------------------------------------------------->
-      <?php require "includes/header_nav.php";?>
-      <article>
-        <figure>
-            <img src="https://placehold.co/1000x100/000000/FFF" alt="">
-        </figure>
+        <!------------------------------------------------- Chamando Header ------------------------------------------------->
+        <?php require "includes/header_nav.php"; ?>
+        <article>
+            <hr>
+            <figure>
+                <img src="https://placehold.co/1000x100/000000/FFF" alt="">
+            </figure>
+            <hr>
 
-        <h1>Daniel Esteves</h1>
-        <a href="https://www.instagram.com/zapata.edicoes?igsh=MXA4YjFycDRibXpiYQ=="><b>@zapata.edicoes</b></a>
+            <h1>Daniel Esteves</h1>
+            <a href="https://www.instagram.com/zapata.edicoes?igsh=MXA4YjFycDRibXpiYQ==" target="_blank"><b>@zapata.edicoes</b></a>
+            <hr>
 
-        <figure>
-            <img src="https://placehold.co/500x500/000000/FFF" alt="">
-        </figure>
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <figure>
+                        <img src="https://placehold.co/500x500/000000/FFF" alt="">
+                    </figure>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
 
-        <p>
-            É o talentoso criador da série <strong>"Nanquim Descartável"</strong>, uma obra que cativou os leitores durante seu período de publicação entre 2007 e 2015. Com roteiros escritos por ele mesmo e ilustrações de uma equipe diversificada, incluindo nomes como Wanderson de Souza, Wagner de Souza, Alex Rodrigues, Mario Cau, Júlio Brilha, Mário César e Fred Hildebrand, a série conquistou reconhecimento pelo seu estilo único e narrativas envolventes.
-        </p>
-        <p>
-            Em 2013, expandiu seu universo criativo com o lançamento do primeiro volume da série "São Paulo dos Mortos", uma visão distópica de um futuro próximo dominado por zumbis.
-        </p>
-        <p>
-            Demonstrando sua versatilidade como autor, em 2019, ele presenteou os leitores com o romance gráfico "Último Assalto", ilustrado por Alex Rodrigues. Esta obra foi indicada ao prestigiado <strong>Prêmio Jabuti na categoria de melhor história em quadrinhos</strong> no ano seguinte.
-        </p>
-        <p>
-            Além de sua prolífica carreira como autor de quadrinhos, Daniel Esteves é também um empreendedor e educador dedicado. Ele fundou a editora independente Zapata Edições e criou o curso "HQ em Foco", ampliando seu impacto na cena cultural. Sua participação ativa nos coletivos Petisco e Quarto Mundo também destaca seu compromisso em fortalecer e enriquecer o cenário dos quadrinhos brasileiros. 
-        </p>
-        <figure>
-            <img src="https://placehold.co/1200x300/000000/FFF" alt="">
-        </figure>
-      </article>
+                    <p>
+                        É o talentoso criador da série <strong>"Nanquim Descartável"</strong>, uma obra que cativou os leitores durante seu período de publicação entre 2007 e 2015. Com roteiros escritos por ele mesmo e ilustrações de uma equipe diversificada, incluindo nomes como Wanderson de Souza, Wagner de Souza, Alex Rodrigues, Mario Cau, Júlio Brilha, Mário César e Fred Hildebrand, a série conquistou reconhecimento pelo seu estilo único e narrativas envolventes.
+                    </p>
+                    <p>
+                        Em 2013, expandiu seu universo criativo com o lançamento do primeiro volume da série "São Paulo dos Mortos", uma visão distópica de um futuro próximo dominado por zumbis.
+                    </p>
+                    <p>
+                        Demonstrando sua versatilidade como autor, em 2019, ele presenteou os leitores com o romance gráfico "Último Assalto", ilustrado por Alex Rodrigues. Esta obra foi indicada ao prestigiado <strong>Prêmio Jabuti na categoria de melhor história em quadrinhos</strong> no ano seguinte.
+                    </p>
+                    <p>
+                        Além de sua prolífica carreira como autor de quadrinhos, Daniel Esteves é também um empreendedor e educador dedicado. Ele fundou a editora independente Zapata Edições e criou o curso "HQ em Foco", ampliando seu impacto na cena cultural. Sua participação ativa nos coletivos Petisco e Quarto Mundo também destaca seu compromisso em fortalecer e enriquecer o cenário dos quadrinhos brasileiros.
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <figure>
+                <img src="https://placehold.co/1200x300/000000/FFF" alt="">
+            </figure>
+            <hr>
 
-<!------------------------------------------------- Chamando Footer ------------------------------------------------->
-      <?php require "includes/footer.php";?>
+        </article>
 
-<!------------------------------------------------- Chamando Java ------------------------------------------------->
-      <?php require "includes/java.php";?>
+        <!------------------------------------------------- Chamando Footer ------------------------------------------------->
+        <?php require "includes/footer.php"; ?>
+
+        <!------------------------------------------------- Chamando Java ------------------------------------------------->
+        <?php require "includes/java.php"; ?>
 
     </div>
-  </body>
+</body>
--- jefferson.php
+++ jefferson.php
@@ -1,51 +1,64 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
-<?php require "includes/head.php";?>
+<?php require "includes/head.php"; ?>
 
 <title>Jefferson Costa </title>
 
-  <body>
+<body>
     <div class="container text-center">
-<!------------------------------------------------- Chamando Header ------------------------------------------------->
-      <?php require "includes/header_nav.php";?>
-      <article>
-        <figure>
-            <img src="https://placehold.co/1000x100/000000/FFF" alt="">
-        </figure>
+        <!------------------------------------------------- Chamando Header ------------------------------------------------->
+        <?php require "includes/header_nav.php"; ?>
+        <article>
+            <hr>
+            <figure>
+                <img src="https://placehold.co/1000x100/000000/FFF" alt="">
+            </figure>
+            <hr>
 
-        <h1>Jefferson Costa </h1>
-        <a href="https://www.instagram.com/jeff_costa_rm?igsh=Z3IxbnE4ZGpjYWUz"><b>@jeff_costa_rm</b></a>
+            <h1>Jefferson Costa </h1>
+            <a href="https://www.instagram.com/jeff_costa_rm?igsh=Z3IxbnE4ZGpjYWUz" target="_blank"><b>@jeff_costa_rm</b></a>
+            <hr>
 
-        <figure>
-            <img src="https://placehold.co/500x500/000000/FFF" alt="">
-        </figure>
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <figure>
+                        <img src="https://placehold.co/500x500/000000/FFF" alt="">
+                    </figure>
+                </div>
 
-        <p>
-            Nascido em São Paulo/SP em 1979, é um artista versátil que tem deixado sua marca em diversas áreas, incluindo ilustração, storyboard, e desenho de personagens e cenários para animações de destaque. Sua criatividade e talento foram fundamentais para produções aclamadas como “Historietas Assombradas (para Crianças Malcriadas)” para o Cartoon Network e “As Microaventuras de Tito e Muda” para o Discovery Kids Brasil, <strong>esta última premiada como Melhor Série no Festival Anima Latina, na Argentina.</strong>
-        </p>
-        <p>
-            Além disso, contribuí como desenhista e animador em séries memoráveis como “Megaliga”, “Fudêncio”, “Infortúnio”, “Peruíbe Salva”, “The Jorges” e “Rockstarghost”, todas exibidas pela MTV.
-        </p>
-        <p>
-            Como quadrinista, deixou sua marca em diversas coletâneas, incluindo “Front #16” e “Quebra Queixo - Technorama”, além de participar dos projetos “Bang-Bang”, “Máquina Zero” e “Gibi Quântico”. Ilustrou álbuns notáveis como “A Dama do Martinelli” em parceria com Marcela Godoy, e “A Tempestade” de Shakespeare, vencedor do <strong>HQMIX 2013 na categoria Adaptação para os Quadrinhos</strong>. Outro trabalho destacado foi “La Dansarina”, que recebeu os prêmios de <strong>Melhor Edição Especial e Melhor Roteiro no HQMIix 2015</strong>.
-        </p>
-        <p>
-            Sob o selo <strong>Graphic MSP, lançou “Jeremias - Pele” (2018 - vencedor do Prêmio Jabuti e do Troféu HQMix 2019)</strong> e <strong>“Jeremias - Alma” (2020 - Troféu HQMix 2021)</strong>. Em 2019, escreveu e desenhou <strong>“Roseira, Medalha, Engenho e outras histórias”, premiado com o Troféu HQMix 2020</strong>, e lançou o livro ilustrado “Anansi”, escrito por Lucas Benetti. 
-        </p>
-        <p>
-            No mercado internacional, adaptou para quadrinhos o livro “Kiss me Judas” e a antologia “Gunned Down”, ambos pela Terra Major nos EUA. Contribuí com a antologia de western “Outlaw Territory 3” para a Image, e para a Edebê, trabalhou na graphic novel “Arcane Sally and Mr. Steam”, escrita por David Hedges. Atualmente, está envolvido na produção da biografia de Fela Kuti, escrita por Nathalia Thomaz.
-        </p>
-        <figure>
-            <img src="https://placehold.co/1200x300/000000/FFF" alt="">
-        </figure>
-      </article>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <p>
+                        Nascido em São Paulo/SP em 1979, é um artista versátil que tem deixado sua marca em diversas áreas, incluindo ilustração, storyboard, e desenho de personagens e cenários para animações de destaque. Sua criatividade e talento foram fundamentais para produções aclamadas como “Historietas Assombradas (para Crianças Malcriadas)” para o Cartoon Network e “As Microaventuras de Tito e Muda” para o Discovery Kids Brasil, <strong>esta última premiada como Melhor Série no Festival Anima Latina, na Argentina.</strong>
+                    </p>
+                    <p>
+                        Além disso, contribuí como desenhista e animador em séries memoráveis como “Megaliga”, “Fudêncio”, “Infortúnio”, “Peruíbe Salva”, “The Jorges” e “Rockstarghost”, todas exibidas pela MTV.
+                    </p>
+                    <p>
+                        Como quadrinista, deixou sua marca em diversas coletâneas, incluindo “Front #16” e “Quebra Queixo - Technorama”, além de participar dos projetos “Bang-Bang”, “Máquina Zero” e “Gibi Quântico”. Ilustrou álbuns notáveis como “A Dama do Martinelli” em parceria com Marcela Godoy, e “A Tempestade” de Shakespeare, vencedor do <strong>HQMIX 2013 na categoria Adaptação para os Quadrinhos</strong>. Outro trabalho destacado foi “La Dansarina”, que recebeu os prêmios de <strong>Melhor Edição Especial e Melhor Roteiro no HQMIix 2015</strong>.
+                    </p>
+                    <p>
+                        Sob o selo <strong>Graphic MSP, lançou “Jeremias - Pele” (2018 - vencedor do Prêmio Jabuti e do Troféu HQMix 2019)</strong> e <strong>“Jeremias - Alma” (2020 - Troféu HQMix 2021)</strong>. Em 2019, escreveu e desenhou <strong>“Roseira, Medalha, Engenho e outras histórias”, premiado com o Troféu HQMix 2020</strong>, e lançou o livro ilustrado “Anansi”, escrito por Lucas Benetti.
+                    </p>
+                    <p>
+                        No mercado internacional, adaptou para quadrinhos o livro “Kiss me Judas” e a antologia “Gunned Down”, ambos pela Terra Major nos EUA. Contribuí com a antologia de western “Outlaw Territory 3” para a Image, e para a Edebê, trabalhou na graphic novel “Arcane Sally and Mr. Steam”, escrita por David Hedges. Atualmente, está envolvido na produção da biografia de Fela Kuti, escrita por Nathalia Thomaz.
+                    </p>
+                </div>
+            </div>
 
-<!------------------------------------------------- Chamando Footer ------------------------------------------------->
-      <?php require "includes/footer.php";?>
+            <hr>
+            <figure>
+                <img src="https://placehold.co/1200x300/000000/FFF" alt="">
+            </figure>
+            <hr>
 
-<!------------------------------------------------- Chamando Java ------------------------------------------------->
-      <?php require "includes/java.php";?>
+        </article>
+
+        <!------------------------------------------------- Chamando Footer ------------------------------------------------->
+        <?php require "includes/footer.php"; ?>
+
+        <!------------------------------------------------- Chamando Java ------------------------------------------------->
+        <?php require "includes/java.php"; ?>
 
     </div>
-  </body>
+</body>

cfffc953705cf3914e2d4f89b59a178f51575a85
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-14 18:03:54 -0300

    Tentando arrumar algumas coisas para a tela tablet

--- contato.php
+++ contato.php
@@ -14,7 +14,7 @@
 
       <div class="row mt-3 justify-content-center">
 
-        <div class="col-lg-4 col-md-12">
+        <div class="col-lg-4 col-md-4 col-sm-12">
           <h1>Fale com a gente!</h1>
           <hr>
           <form action="sucesso.php" method="post">
@@ -26,7 +26,7 @@
           </form>
         </div>
 
-        <div class="col-lg-6 col-md-12 mt-3">
+        <div class="col-lg-6 col-md-6 col-sm-12 mt-3">
           <img src="imagens/fatecon_logo.webp" alt="Mascote do evento FateconHQs, um garoto com traços de desenho, cabelo, manga longa, luvas, calças e óculos redondo azuis, camisa peitoral laranja escuro com claro, rosto amarelado e jetpack de foguete nas costas. Ele está impressionado com as informações ao redor e diz 'uau... que DEMAIS!'. O fundo tem cores vibrantes e animadas com estrelas, cores rosa, roxo e detalhes verdes." class="img-fluid">
         </div>
 

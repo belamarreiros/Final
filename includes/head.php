c72ce5c43e51dbe7c12b1b59e3a03704a6e80b3b
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-14 11:04:08 -0300

    Consertando bugs

--- includes/head.php
+++ includes/head.php
@@ -3,14 +3,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/meu.css">
 
   <!-- Bootstrap CSS v5.2.1 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
   <!-- Bootstrap Icons via CDN -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
@@ -25,4 +23,3 @@
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 
 </head>
-
--- premiacoes.php
+++ premiacoes.php
@@ -2,111 +2,116 @@
 <html lang="pt-BR">
 
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
-<?php require "includes/head.php";?>
+<?php require "includes/head.php"; ?>
 
 <title>PREMIAÇÕES</title>
 
 <body>
-<!------------------------------------------------- Chamando Header ------------------------------------------------->
-  <div class="container text-center">
-  <?php require "includes/header_nav.php";?>
-  <hr>
+    <!------------------------------------------------- Chamando Header ------------------------------------------------->
+    <div class="container text-center">
+        <?php require "includes/header_nav.php"; ?>
+        <hr>
 
-    <article>
-        <h1>2023</h1>
-        <h2>Desenhos:</h2>  
-    </article>
-    <section class="mt-3">
+        <article>
+            <h1>2023</h1>
+            <h2>Desenhos:</h2>
+        </article>
+        <section class="mt-3">
 
-    <div class="row justify-content-center mt-3">
-        <div class="col-lg-3 col-md-12 col-sm-12">
-            <img src="imagens/desenho1lugar_2023.webp" alt="A imagem é uma obra de arte colorida que inclui vários elementos e parece ser um certificado de vencedor ou prêmio. No topo, há um texto que diz “SÃO ROQUE” com uma fonte estilizada e elementos decorativos ao redor. A parte central da imagem apresenta uma ilustração de uma figura feminina com cabelos longos cercada por folhas, galhos e o que parecem ser videiras, indicando um possível tema relacionado à natureza ou vinícolas. Abaixo desta figura, há várias ilustrações menores retratando prédios que lembram a arquitetura europeia, garrafas de vinho com rótulos como “Vinhos Góes” e “Mila Don Pato”, e cachos de uvas. Esses elementos sugerem que a imagem pode estar relacionada à produção de vinho ou a uma região conhecida por suas vinícolas.
+            <div class="row justify-content-center mt-3">
+                <div class="col-lg-3 col-md-12 col-sm-12">
+                    <img src="imagens/desenho1lugar_2023.webp" alt="A imagem é uma obra de arte colorida que inclui vários elementos e parece ser um certificado de vencedor ou prêmio. No topo, há um texto que diz “SÃO ROQUE” com uma fonte estilizada e elementos decorativos ao redor. A parte central da imagem apresenta uma ilustração de uma figura feminina com cabelos longos cercada por folhas, galhos e o que parecem ser videiras, indicando um possível tema relacionado à natureza ou vinícolas. Abaixo desta figura, há várias ilustrações menores retratando prédios que lembram a arquitetura europeia, garrafas de vinho com rótulos como “Vinhos Góes” e “Mila Don Pato”, e cachos de uvas. Esses elementos sugerem que a imagem pode estar relacionada à produção de vinho ou a uma região conhecida por suas vinícolas.
 
 No canto inferior esquerdo, há um texto proeminente que diz “PRIMEIRO LUGAR” em letras maiúsculas em negrito sobre um fundo rosa, o que sugere que a obra de arte recebeu o primeiro lugar em alguma forma de competição. Além disso, há um texto identificando a artista como “EMILLY YUKARI BASTOS MARUYAMA, 16 anos”, indicando que a artista tem 16 anos de idade." class="img-fluid">
-            <h2>1º Lugar</h2>
-        </div>
-    
-        <div class="col-lg-3 col-md-6 col-sm-12">
-            <img src="imagens/desenho2lugar_2023.webp" alt="A imagem é um desenho colorido que parece ser inspirado na “Noite Estrelada” de Vincent Van Gogh. Ela apresenta padrões giratórios no céu com estrelas e uma lua crescente, imitando o estilo icônico de Van Gogh. No primeiro plano, há uma estrutura proeminente que se assemelha a igraja da praça da matriz de São Roque-SP, junto com outros prédios que representam a cidade ou paisagem urbana. As cores são vibrantes, com azuis e amarelos dominando a paleta. No topo da imagem, o texto lê “SEGUNDO LUGAR”. Abaixo deste texto está outra linha que diz “NATHALIA GARCIA DE ALMEIDA, 15 anos”, indicando o nome e a idade do artista." class="img-fluid">
-            <h2>2º Lugar</h2>
-        </div>
-    
-        <div class="col-lg-3 col-md-6 col-sm-12">
-            <img src="imagens/desenho3lugar_2023.webp" alt="A imagem é um desenho colorido que apresenta uma paisagem urbana com vários elementos. No centro, há uma grande colina ou montanha coberta de árvores com prédios de diferentes tamanhos e designs ao redor. O céu acima exibe um gradiente de roxo para amarelo, sugerindo o amanhecer ou o anoitecer. Há também algumas nuvens e o que parece ser um planeta ou lua visíveis no céu. No primeiro plano, há uma estrada com veículos, incluindo um ônibus amarelo proeminente no lado esquerdo. O desenho tem uma sensação geral vibrante e um pouco futurista devido à representação estilizada da paisagem urbana.
+                    <h2>1º Lugar</h2>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <img src="imagens/desenho2lugar_2023.webp" alt="A imagem é um desenho colorido que parece ser inspirado na “Noite Estrelada” de Vincent Van Gogh. Ela apresenta padrões giratórios no céu com estrelas e uma lua crescente, imitando o estilo icônico de Van Gogh. No primeiro plano, há uma estrutura proeminente que se assemelha a igraja da praça da matriz de São Roque-SP, junto com outros prédios que representam a cidade ou paisagem urbana. As cores são vibrantes, com azuis e amarelos dominando a paleta. No topo da imagem, o texto lê “SEGUNDO LUGAR”. Abaixo deste texto está outra linha que diz “NATHALIA GARCIA DE ALMEIDA, 15 anos”, indicando o nome e a idade do artista." class="img-fluid">
+                    <h2>2º Lugar</h2>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <img src="imagens/desenho3lugar_2023.webp" alt="A imagem é um desenho colorido que apresenta uma paisagem urbana com vários elementos. No centro, há uma grande colina ou montanha coberta de árvores com prédios de diferentes tamanhos e designs ao redor. O céu acima exibe um gradiente de roxo para amarelo, sugerindo o amanhecer ou o anoitecer. Há também algumas nuvens e o que parece ser um planeta ou lua visíveis no céu. No primeiro plano, há uma estrada com veículos, incluindo um ônibus amarelo proeminente no lado esquerdo. O desenho tem uma sensação geral vibrante e um pouco futurista devido à representação estilizada da paisagem urbana.
 
 No topo da imagem, em letras negrito contra um fundo roxo, está escrito “TERCEIRO LUGAR”. Abaixo deste texto está outra linha que diz “MIGUEL DE OLIVEIRA ASSÊNCIO DUTRA, 17 ANOS”, que indica que esta obra de arte alcançou o terceiro lugar em uma competição e fornece o nome e a idade do artista." class="img-fluid">
-            <h2>3º Lugar</h2>
-        </div>
+                    <h2>3º Lugar</h2>
+                </div>
 
-      </div>
-
-      <div class="row justify-content-center mt-3">
-        <div class="col-lg-10 col-md-6 col-sm-12">
-            <quote><p>"A FATEC São Roque e o jornal O Democrata têm o prazer de anunciar os vencedores do Concurso Desenhe São Roque, realizado durante a FATECon HQs, após um processo de seleção criterioso. O júri, composto pelos artistas participantes do evento, teve a difícil tarefa de escolher seus favoritos entre as incríveis 255 inscrições recebidas.</p>
-
-            <p>Após cuidadosa deliberação, foram selecionados 21 finalistas, cujas obras ficaram em exibição nos dias 12 e 13 de maio, durante a realização da FATECon HQs. Os frequentadores do evento tiveram a oportunidade de votar em suas obras favoritas, contribuindo para a definição dos vencedores.</p>
-
-            <p>É com grande satisfação que anunciamos os resultados finais, baseados em 440 votos apurados:</p>
-            <p>🥇 1° Lugar: Inscrição 199 - Emilly Yukari Bastos Maruyama, 16 anos.</p>
-            <p>🥈 2° Lugar: Inscrição 29 - Nathalia Garcia de Almeida, 15 anos.</p>
-            <p>🥉 3° Lugar: Inscrição 30 - Miguel de Oliveira Assêncio Dutra, 17 anos.</p>
-            <p>Parabenizamos todos os participantes pelo talento e dedicação demonstrados em suas obras. Agradecemos também aos artistas que compuseram o júri, aos frequentadores da FATECon HQs que contribuíram com suas votações, bem como aos nossos patrocinadores: a Editora Guará, o aplicativo FunkToon e a escola de quadrinhos Inko Criativo.</p>
-            <p>Os três vencedores serão premiados com assinaturas premium do FunkToon. A segunda colocada também receberá uma coleção de gibis da Editora Guará, enquanto a grande vencedora levará para casa uma bolsa de 6 meses no curso online de quadrinhos ou ilustração da Inko Criativo.</p>
-            <p>O Concurso Desenhe São Roque foi um sucesso graças ao apoio e envolvimento de cada um de vocês. Continuem explorando sua criatividade, espalhando a arte por onde forem e, principalmente, leiam gibis! 😉"</p></quote>
-        </div>
-
-      </div>
-
-    </section>
-    <hr>
-    <article>
-        <h1>2023</h1>
-        <h2>Cosplays:</h2>  
-    </article>
-    <section class="mt-3">
-
-        <div class="row justify-content-center">
-
-            <div class="col-lg-3 col-md-12 col-sm-12">
-                <img src="imagens/cosplay1lugar_2023.webp" alt="Uma foto em estilo de montagem com o mascote Fatecon e o primeiro lugar do concurso de Cosplays. Um menino vestido com roupas marrons que cobrem a cabeça e uma espada em forma de serra." class="img-fluid">
-                <h2>1º Lugar</h2>
-            </div>
-    
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img src="imagens/cosplay2lugar_2023.webp" alt="Uma foto em estilo de montagem com o mascote Fatecon e o segundo lugar do concurso de Cosplays. Um menino vestido com roupas simples, calça preta e camisa branca, com um capacete e várias serras como se fossem serras elétricas" class="img-fluid">
-                <h2>2º Lugar</h2>
-            </div>
-    
-            <div class="col-lg-3 col-md-6 col-sm-12">
-                <img src="imagens/cosplay3lugar_2023.webp" alt="Uma foto em estilo de montagem com o mascote Fatecon e o terceiro lugar do concurso de Cosplays. Uma menina loira com roupas de anime." class="img-fluid">
-                <h2>3º Lugar</h2>
             </div>
 
-        </div>
+            <div class="row justify-content-center mt-3">
+                <div class="col-lg-10 col-md-6 col-sm-12">
+                    <quote>
+                        <p>"A FATEC São Roque e o jornal O Democrata têm o prazer de anunciar os vencedores do Concurso Desenhe São Roque, realizado durante a FATECon HQs, após um processo de seleção criterioso. O júri, composto pelos artistas participantes do evento, teve a difícil tarefa de escolher seus favoritos entre as incríveis 255 inscrições recebidas.</p>
 
-    <div class="row justify-content-center mt-3">
-        <div class="col-lg-10 col-md-6 col-sm-12">
+                        <p>Após cuidadosa deliberação, foram selecionados 21 finalistas, cujas obras ficaram em exibição nos dias 12 e 13 de maio, durante a realização da FATECon HQs. Os frequentadores do evento tiveram a oportunidade de votar em suas obras favoritas, contribuindo para a definição dos vencedores.</p>
 
-            <quote><p>"🎉🎉🎉 Parabéns aos vencedores do nosso Desfile Cosplay FATECon HQs 2023! 🎉🎉🎉</p>
-            <p>🥇 No primeiro lugar temos Bruno Siberi, que nos impressionou com seu Pyramid Head de Silent Hill.</p>
-            <p>🥈 Em segundo lugar, Miguel de G. A. Dutra, com seu incrível Chainsaw Man do Anime/Mangá Chainsaw Man.</p>
-            <p>🥉 E no terceiro lugar, Lawany M. Teles, com a maravilhosa Evelynn de League of Legends.</p>
-            <p>🎁 Os prêmios são: R$300,00 para o primeiro lugar, R$200,00 para o segundo e R$100,00 para o terceiro. Além disso, nosso campeão Bruno Siberi receberá uma sessão de fotos profissional com o talentoso fotógrafo Divanil Urbano (@divanilurbanofotografia).</p>
-            <p>Queremos expressar nosso agradecimento a todos os cosplayers que participaram, vocês trouxeram tanta energia e criatividade para o nosso evento!</p>
-            <p>Obrigado a todos por tornarem a FATECon HQs 2023 um sucesso incrível. Até a próxima! 🚀🌟🙌"</p></quote>
+                        <p>É com grande satisfação que anunciamos os resultados finais, baseados em 440 votos apurados:</p>
+                        <p>🥇 1° Lugar: Inscrição 199 - Emilly Yukari Bastos Maruyama, 16 anos.</p>
+                        <p>🥈 2° Lugar: Inscrição 29 - Nathalia Garcia de Almeida, 15 anos.</p>
+                        <p>🥉 3° Lugar: Inscrição 30 - Miguel de Oliveira Assêncio Dutra, 17 anos.</p>
+                        <p>Parabenizamos todos os participantes pelo talento e dedicação demonstrados em suas obras. Agradecemos também aos artistas que compuseram o júri, aos frequentadores da FATECon HQs que contribuíram com suas votações, bem como aos nossos patrocinadores: a Editora Guará, o aplicativo FunkToon e a escola de quadrinhos Inko Criativo.</p>
+                        <p>Os três vencedores serão premiados com assinaturas premium do FunkToon. A segunda colocada também receberá uma coleção de gibis da Editora Guará, enquanto a grande vencedora levará para casa uma bolsa de 6 meses no curso online de quadrinhos ou ilustração da Inko Criativo.</p>
+                        <p>O Concurso Desenhe São Roque foi um sucesso graças ao apoio e envolvimento de cada um de vocês. Continuem explorando sua criatividade, espalhando a arte por onde forem e, principalmente, leiam gibis! 😉"</p>
+                    </quote>
+                </div>
 
-            <p><small><small>*Não existem registros dos premiados da primeira edição de 2022.</small></small></p>
-        </div>
+            </div>
+
+        </section>
+        <hr>
+        <article>
+            <h1>2023</h1>
+            <h2>Cosplays:</h2>
+        </article>
+        <section class="mt-3">
+
+            <div class="row justify-content-center">
+
+                <div class="col-lg-3 col-md-12 col-sm-12">
+                    <img src="imagens/cosplay1lugar_2023.webp" alt="Uma foto em estilo de montagem com o mascote Fatecon e o primeiro lugar do concurso de Cosplays. Um menino vestido com roupas marrons que cobrem a cabeça e uma espada em forma de serra." class="img-fluid">
+                    <h2>1º Lugar</h2>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <img src="imagens/cosplay2lugar_2023.webp" alt="Uma foto em estilo de montagem com o mascote Fatecon e o segundo lugar do concurso de Cosplays. Um menino vestido com roupas simples, calça preta e camisa branca, com um capacete e várias serras como se fossem serras elétricas" class="img-fluid">
+                    <h2>2º Lugar</h2>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    <img src="imagens/cosplay3lugar_2023.webp" alt="Uma foto em estilo de montagem com o mascote Fatecon e o terceiro lugar do concurso de Cosplays. Uma menina loira com roupas de anime." class="img-fluid">
+                    <h2>3º Lugar</h2>
+                </div>
+
+            </div>
+
+            <div class="row justify-content-center mt-3">
+                <div class="col-lg-10 col-md-6 col-sm-12">
+
+                    <quote>
+                        <p>"🎉🎉🎉 Parabéns aos vencedores do nosso Desfile Cosplay FATECon HQs 2023! 🎉🎉🎉</p>
+                        <p>🥇 No primeiro lugar temos Bruno Siberi, que nos impressionou com seu Pyramid Head de Silent Hill.</p>
+                        <p>🥈 Em segundo lugar, Miguel de G. A. Dutra, com seu incrível Chainsaw Man do Anime/Mangá Chainsaw Man.</p>
+                        <p>🥉 E no terceiro lugar, Lawany M. Teles, com a maravilhosa Evelynn de League of Legends.</p>
+                        <p>🎁 Os prêmios são: R$300,00 para o primeiro lugar, R$200,00 para o segundo e R$100,00 para o terceiro. Além disso, nosso campeão Bruno Siberi receberá uma sessão de fotos profissional com o talentoso fotógrafo Divanil Urbano (@divanilurbanofotografia).</p>
+                        <p>Queremos expressar nosso agradecimento a todos os cosplayers que participaram, vocês trouxeram tanta energia e criatividade para o nosso evento!</p>
+                        <p>Obrigado a todos por tornarem a FATECon HQs 2023 um sucesso incrível. Até a próxima! 🚀🌟🙌"</p>
+                    </quote>
+
+                    <p><small><small>*Não existem registros dos premiados da primeira edição de 2022.</small></small></p>
+                </div>
+            </div>
+
+        </section>
+        <hr>
+        <!------------------------------------------------- Chamando Footer ------------------------------------------------->
+        <?php require "includes/footer.php"; ?>
+
+        <!------------------------------------------------- Chamando Java ------------------------------------------------->
+        <?php require "includes/java.php"; ?>
+
     </div>
-
-    </section>
-    <hr>
-<!------------------------------------------------- Chamando Footer ------------------------------------------------->
-    <?php require "includes/footer.php";?>
-
-<!------------------------------------------------- Chamando Java ------------------------------------------------->
-    <?php require "includes/java.php";?>
-    
-  </div>
 </body>
+
 </html>

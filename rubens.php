fcefa3833eb8d7df1d95d820fd653030ee8c9a3f
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-15 22:16:34 -0300

    finalizando a página do rubens

--- rubens.php
+++ rubens.php
@@ -10,44 +10,59 @@
         <!------------------------------------------------- Chamando Header ------------------------------------------------->
         <?php require "includes/header_nav.php"; ?>
         <article class="mt-3">
+            <hr>
             <div class="row mt-3 justify-content-center">
-                <hr>
-                <div class="col-lg-10 col-md-8 col-sm-9">
+                <div class="col-lg-6 col-md-6 col-sm-9">
+                    <figure>
+                        <img src="imagens/rubens1.webp" alt="" class="img-fluid">
+                    </figure>
+                </div>
+                <div class="col-lg-5 col-md-6 col-sm-9">
                     <h3>Por que tem uma página sobre o Rubens?</h3>
                     <br>
                     <p>
-                        Rubens Menezes é simplesmente o idealizador e criador da Fatecon. Não tinha como não fazermos uma página especial para ele.
+                        Rubens Menezes é mais do que apenas o idealizador da Fatecon; ele é a alma e a inspiração por trás deste evento anual que celebra a arte dos quadrinhos. Desde a sua concepção, a Fatecon tem sido um ponto de encontro vibrante para cartunistas e entusiastas da nona arte, e nada disso seria possível sem a visão e o entusiasmo incansável de Rubens.
                     </p>
                     <p>
-                        Mostraremos aqui um pouco de sua história e também alguns de seus quadrinhos.
+                        Mostraremos aqui um pouco de sua história e também alguns de seus quadrinhos. Através desta página, esperamos não só contar a história de Rubens, mas também inspirar futuros artistas e entusiastas a seguir suas próprias paixões.
                     </p>
                 </div>
 
             </div>
             <hr>
-
-            <div class="row mt-3 justify-content-center">
-                <div class="col-lg-4 col-md-8 col-sm-9">
-                    <figure>
-                        <img src="imagens/rubens1.webp" alt="" class="img-fluid">
-                    </figure>
-                </div>
-
-            </div>
+            <figure>
+                <img src="imagens/banner_rubens.svg" alt="" class="img-banner">
+            </figure>
+            <hr>
 
             <div class="row mt-3 justify-content-center">
 
-                <div class="col-lg-10 col-md-8 col-sm-9">
+                <div class="col-lg-6 col-md-8 col-sm-9">
                     <h2> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16" style="margin-right: 10px;">
                             <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205l-.014-.058-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5s-1.411-.136-2.025-.267c-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a30 30 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5q.001-.264.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085q.084.236.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5" />
                         </svg>O Rubens</h2>
                     </h2>
                     <p>
-                        Rubens Araújo Menezes de Souza Filho é um estudioso e pesquisador brasileiro. Ele possui um doutorado em História Social pela Universidade de São Paulo (USP), obtido em 2014. Sua tese de doutorado, intitulada <strong>“Os Donos das Ideias - História e Conflitos do Direito Autoral, do Copyright e das Patentes na Crise Contemporânea do Capital: da Rodada Uruguai (1986) aos Partidos Piratas (2006)”</strong>, explora questões de propriedade intelectual, direitos autorais e patentes no contexto do desenvolvimento tecnológico e do capitalismo . Seus interesses de pesquisa incluem Estudos de Ciência e Tecnologia (CTS), patentes de software, software de código aberto e o impacto da tecnologia na sociedade.
+                        Rubens Araújo Menezes de Souza Filho é um estudioso e pesquisador brasileiro. Ele possui um doutorado em História Social pela Universidade de São Paulo (USP), obtido em 2014. Sua tese de doutorado explora questões de propriedade intelectual, direitos autorais e patentes no contexto do desenvolvimento tecnológico e do capitalismo . Seus interesses de pesquisa incluem Estudos de Ciência e Tecnologia (CTS), patentes de software, software de código aberto e o impacto da tecnologia na sociedade.
                     </p>
                     <p>
                         Além de seu doutorado, Rubens Araújo Menezes de Souza Filho também possui um mestrado em História Social pela USP, obtido em 2006. Sua dissertação de mestrado focou nas contradições e conflitos do desenvolvimento tecnológico, examinando especificamente o impacto do software de código aberto no Brasil.
                     </p>
+                </div>
+                <div class="col-lg-5 col-md-4 col-sm-9">
+                    <figure>
+                        <img src="imagens/rubens2.webp" alt="" class="img-fluid">
+                    </figure>
+                </div>
+                <hr>
+
+
+                <div class="col-lg-4 col-md-4 col-sm-9">
+                    <figure>
+                        <img src="imagens/rubens3.webp" alt="" class="img-fluid">
+                    </figure>
+                </div>
+                <div class="col-lg-5 col-md-8 col-sm-9">
                     <p>
                         Como acadêmico, ele é professor no Centro Estadual de Educação Tecnológica Paula Souza em São Roque, Brasil, desde 2006.
                     </p>
@@ -59,30 +74,27 @@
             </div>
             <hr>
 
-            <div class="row justify-content-center mb-3">
-
-                <div class="col-lg-2 col-md-12 col-sm-12">
+            <div class="row justify-content-center mt-3">
+                <h2>Quadrinhos:</h2>
+                <div class="col-lg-3 col-md-4 col-sm-10 mt-5">
                     <img src="imagens/urubus.webp" alt="Imagem com Placeholder" class="img-fluid">
                 </div>
-                <div class="col-lg-2 col-md-12 col-sm-12">
-                    <img src="imagens/mexericas.webp" alt="Imagem com Placeholder" class="img-fluid">
-                </div>
-                <div class="col-lg-2 col-md-12 col-sm-12">
-                    <img src="imagens/consumidores.webp" alt="Imagem com Placeholder" class="img-fluid">
-                </div>
-
-            </div>
-
-            <div class="row justify-content-center">
-
-                <div class="col-lg-10 col-md-12 col-sm-12">
-                    <h2>Quadrinhos:</h2>
+                <div class="col-lg-8 col-md-8 col-sm-12 mt-3">
                     <p>
                         <b>- Urubus lançada na CCXP 2023:</b>
                     </p>
                     <p>
                         É uma narrativa de realismo mágico que, em uma metáfora social, aborda temas como desigualdade, ganância, inveja e redenção. A narrativa com um toque de ironia dá aos marginalizados da sociedade um “dom” que os ricos e abastados nunca poderão ter se continuarem sendo quem são. “Urubus” é uma história que atua tanto como um espelho da realidade quanto uma crítica a ela, em uma trama onde eventos inesperados podem levar à revisão profunda de prioridades e percepções. Em um cenário onde as regras conhecidas são desafiadas e as hierarquias sociais são invertidas, “Urubus” convida à reflexão sobre os defeitos do materialismo e a instabilidade das estruturas sociais.
                     </p>
+                </div>
+
+            </div>
+
+            <div class="row justify-content-center mt-3">
+                <div class="col-lg-3 col-md-4 col-sm-10 mt-5">
+                    <img src="imagens/mexericas.webp" alt="Imagem com Placeholder" class="img-fluid">
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 mt-3">
                     <p>
                         <b>- Mexericas lançada na CCXP 2022:</b>
                     </p>
@@ -90,6 +102,15 @@
                         Ambientada em uma escola pública de Osasco, em São Paulo, Mexericas é um retrato da duríssima realidade da educação em um país onde pouca gente se importa de verdade com o assunto. É uma reflexão, na forma de história em quadrinhos, sobre o valor social da educação, e sobre o papel das escolas e famílias na formação das crianças. Também é a história sobre duas meninas, sobre os sonhos que são permitidos a cada um de nós, sobre livros e, claro, sobre mexericas.
                     </p>
                     <br>
+                </div>
+
+            </div>
+
+            <div class="row justify-content-center mt-3">
+                <div class="col-lg-3 col-md-4 col-sm-10 mt-5">
+                    <img src="imagens/consumidores.webp" alt="Imagem com Placeholder" class="img-fluid">
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-12 mt-3">
                     <p>
                         <b>- A primeira HQ de Rubens Menezes, lançada em dezembro de 2019.</b>
                     </p>
@@ -105,12 +126,21 @@
                 </div>
 
             </div>
+
             <hr>
 
             <div class="row mt-3 justify-content-center">
+                <h2>Desenhos:</h2>
 
-                <div class="col-lg-4 col-md-12 col-sm-12">
-                    <img src="imagens/desenho_rubens1.webp" alt="Imagem com Placeholder" class="img-fluid">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <figure>
+                        <img src="imagens/desenhos_rubens.svg" alt="" class="img-fluid">
+                    </figure>
+                </div>
+                <div class="col-lg-7 col-md-8 col-sm-10">
+                    <figure>
+                        <img src="imagens/desenho_nos.webp" alt="" class="img-fluid">
+                    </figure>
                 </div>
 
             </div>
@@ -118,16 +148,18 @@
             <div class="row mt-3 justify-content-center">
 
                 <div class="col-lg-10 col-md-12 col-sm-12">
-                    <h2>Desenhos:</h2>
                     <p>
-                        Rubens Menezes é historiador e professor universitário, que está envolvido com quadrinhos, direta ou indiretamente há mais de 20 anos. Em dezembro de 2019 lançou sua primeira HQ.
+                        Rubens está envolvido com quadrinhos, direta ou indiretamente há mais de 20 anos. Em dezembro de 2019 lançou sua primeira HQ.
                     </p>
                     <p>
-                        Em 2004 ilustrou, com ilustrações vetoriais, cenários para a revista em quadrinhos espanhola Wendy & Rita (link para download), a convite do quadrinista Maxx Figueiredo, que na época desenhava os personagens em primeiro plano, sobre estes cenários.
+                        Em 2004 ilustrou, com ilustrações vetoriais, cenários para a revista em quadrinhos espanhola Wendy & Rita, a convite do quadrinista Maxx Figueiredo, que na época desenhava os personagens em primeiro plano, sobre estes cenários.
                     </p>
                     <p>
                         Entre 2004 e 2006 deu aulas de ilustração vetorial, animação e construção de sites em Flash MX 2004 na Impacta Tecnologia.
                     </p>
+                    <p>
+                        Além de seus vários desenhos esporádicos. No ano de 2023 ele desenhou três dos alunos envolvidos nesse projeto. Na época eram chamados de "Ursinhos sem Calças em Chamas. Os alunos tem um grande apreço por esse desenho e não haveria como finalizar esse projeto sem colocá-lo."
+                    </p>
                 </div>
 
             </div>
@@ -135,15 +167,27 @@
 
             <div class="row mt-3 justify-content-center">
 
-                <div class="col-lg-3 col-md-12 col-sm-12">
+                <div class="col-lg-2 col-md-2 col-sm-8">
                     <img src="imagens/butanta.webp" alt="Imagem com Placeholder" class="img-fluid">
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-8">
+                    <img src="imagens/rubens_fiq22.webp" alt="Imagem com Placeholder" class="img-fluid">
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-8">
+                    <img src="imagens/rubens_ogra.webp" alt="Imagem com Placeholder" class="img-fluid">
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-8">
+                    <img src="imagens/rubens_unesp.webp" alt="Imagem com Placeholder" class="img-fluid">
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-8">
+                    <img src="imagens/rubens_ccx23.webp" alt="Imagem com Placeholder" class="img-fluid">
                 </div>
 
             </div>
 
             <div class="row mt-3 justify-content-center">
 
-                <div class="col-lg-10 col-md-12 col-sm-12">
+                <div class="col-lg-10 col-md-10 col-sm-12">
                     <h2 style="text-align: left">Eventos:</h2>
                     <ul style="text-align: left">
                         <li>01/12/2019 - Butantã Gibi Con, em São Paulo</li>

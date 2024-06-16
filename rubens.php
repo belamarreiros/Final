ecf61a5a133ddf6bedd8116112be57fddbcabc16
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-15 21:10:51 -0300

    Colocando o ícone do Rubens ao lado do nome dele!

--- rubens.php
+++ rubens.php
@@ -38,7 +38,10 @@
             <div class="row mt-3 justify-content-center">
 
                 <div class="col-lg-10 col-md-8 col-sm-9">
-                    <h2>O RUBENS</h2>
+                    <h2> <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16" style="margin-right: 10px;">
+                            <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205l-.014-.058-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5s-1.411-.136-2.025-.267c-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a30 30 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5q.001-.264.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085q.084.236.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5" />
+                        </svg>O Rubens</h2>
+                    </h2>
                     <p>
                         Rubens Araújo Menezes de Souza Filho é um estudioso e pesquisador brasileiro. Ele possui um doutorado em História Social pela Universidade de São Paulo (USP), obtido em 2014. Sua tese de doutorado, intitulada <strong>“Os Donos das Ideias - História e Conflitos do Direito Autoral, do Copyright e das Patentes na Crise Contemporânea do Capital: da Rodada Uruguai (1986) aos Partidos Piratas (2006)”</strong>, explora questões de propriedade intelectual, direitos autorais e patentes no contexto do desenvolvimento tecnológico e do capitalismo . Seus interesses de pesquisa incluem Estudos de Ciência e Tecnologia (CTS), patentes de software, software de código aberto e o impacto da tecnologia na sociedade.
                     </p>

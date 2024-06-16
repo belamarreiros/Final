3066097704482ce87bf47b6c7393c5e04d56d3fb
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-15 21:01:11 -0300

    Muitas e muitas fotos para as páginas fatecon 2022 e 2023. ALém de alguns consertos nos bugs de tamanhos de telas diferentes.

--- fatecon_2022.php
+++ fatecon_2022.php
@@ -3,83 +3,71 @@
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
 <?php require "includes/head.php"; ?>
 
-<title>DESENHOS E COSPLAYS</title>
+<title>FATECON 2022</title>
 
 <body>
   <div class="container text-center">
     <!------------------------------------------------- Chamando Header ------------------------------------------------->
     <?php require "includes/header_nav.php"; ?>
     <article>
-      <h2>Desenhos:</h2>
+      <h2>Veja algumas fotos do evento:</h2>
 
 
 
-      <div class="row">
+      <div class="row justify-content-center">
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_1.webp" width="300" height="300" alt="Mural colorido com um personagem de desenho animado com cabelo azul, óculos grandes, camiseta vermelha e capa preta, sobre um fundo rosa e amarelo com estrelas e outros detalhes decorativos." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_2.webp" width="300" height="300" alt="Grupo de pessoas posando em frente a um banner da Fateccon HQ, a feira de quadrinhos do interior paulista. O banner inclui a imagem de um personagem de desenho animado com cabelo azul, óculos grandes, camiseta vermelha e capa preta." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_3.webp" width="300" height="300" alt="Três pessoas posando em frente a um banner da Fateccon HQ, a feira de quadrinhos do interior paulista. O banner inclui a imagem de um personagem de desenho animado com cabelo azul, óculos grandes, camiseta vermelha e capa preta." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-      </div>
-      <div class="row">
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_4.webp" width="300" height="300" alt="Duas pessoas usando máscaras faciais, segurando bastões coloridos (vermelho e amarelo) e se preparando para um duelo amigável ao ar livre. Uma pessoa veste camiseta branca e calças escuras, enquanto a outra veste camiseta verde com colete azul e shorts pretos. Ao fundo, há um prédio de tijolos com uma área coberta onde outras pessoas estão sentadas." class="img-fluid">
         </div>
       </div>
       <div class="row">
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_5.webp" width="300" height="300" alt="Duas pessoas usando máscaras faciais, participando de um duelo amigável ao ar livre com bastões coloridos. A pessoa à esquerda, vestindo camiseta branca e calças escuras, está em posição inclinada, enquanto a pessoa à direita, vestindo camiseta verde, colete azul e shorts pretos, está em pé e atacando com os bastões vermelho e amarelo. Ao fundo, há um prédio com uma área coberta onde outras pessoas estão sentadas." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_6.webp" width="300" height="300" alt="Um grupo de pessoas posando para uma foto em frente a um mural com a palavra 'FATECON'. Todos estão sorrindo e usando crachás. O mural ao fundo tem um design colorido com um personagem animado. O grupo é diverso, com pessoas em várias poses e expressões, em um ambiente interno." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_7.webp" width="300" height="300" alt="Desenho de um personagem de desenho animado sorrindo e dizendo 'VALEU!' com 'PKS 2022' escrito abaixo. Ao fundo, uma pessoa está sentada desenhando em uma mesa de convenção com uma placa vermelha que diz 'PULP CobrinhaS' e o número '15'." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_8.webp" width="300" height="300" alt="Quatro homens posando juntos e sorrindo em um ambiente interno. O primeiro homem à esquerda está usando uma camiseta vermelha. O segundo homem tem cabelo e barba grisalhos, usa óculos e uma camiseta preta com uma ilustração colorida. O terceiro homem usa um chapéu de palha e uma camiseta preta com um design gráfico. O quarto homem está usando uma camiseta branca com um gráfico." class="img-fluid">
         </div>
       </div>
+      <div class="row">
 
-      <p>Lorem ipsum dolor sit amet consectetur. Vivamus nibh aliquam eget fames. Viverra sed lorem viverra sapien
-        tortor ornare. At fermentum non pellentesque morbi nisi egestas dictumst euismod. Ut vitae rhoncus est blandit
-        lorem aliquam. Fusce volutpat vel elementum duis a nisl sit mauris mauris. Eget donec dolor tincidunt imperdiet
-        pellentesque. Viverra in montes id id pharetra quam commodo quam metus. Cursus aenean fringilla facilisis
-        consequat nunc dolor lacinia potenti aliquet. Fermentum quisque lacus lacus augue. Lorem ipsum dolor sit amet
-        consectetur. Vivamus nibh aliquam eget fames. Viverra sed lorem viverra sapien tortor ornare. At fermentum non
-        pellentesque morbi nisi egestas dictumst euismod. Ut vitae rhoncus est blandit lorem aliquam. Fusce volutpat vel
-        elementum duis a nisl sit mauris mauris. Eget donec dolor tincidunt imperdiet pellentesque. Viverra in montes id
-        id pharetra quam commodo.<br><br><br> </p>
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_9.webp" width="300" height="300" alt="Grande área interna, possivelmente um refeitório ou espaço multiuso, cheio de pessoas sentadas em mesas, interagindo e participando de atividades. Ao fundo, há uma área com mais atividades ou expositores, com pessoas de pé." class="img-fluid">
+        </div>
 
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_10.webp" width="300" height="300" alt="Evento coberto com várias mesas onde pessoas interagem e compram produtos. Há livros e quadrinhos sobre a mesa em primeiro plano. Um grande banner ao fundo diz 'FATECON'." class="img-fluid">
+        </div>
 
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_11.webp" width="300" height="300" alt="Três pessoas sentadas participando de uma discussão ou entrevista, com uma delas segurando um microfone. Ao fundo, uma parede com arte colorida e um grande banner com a palavra 'FATECON'." class="img-fluid">
+        </div>
+
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2022_12.webp" width="300" height="300" alt="Pessoa segurando um livro intitulado 'Os consumidores selvagens e a ascenção dos palhaços'. Ao fundo, outros livros ou quadrinhos estão desfocados, com títulos como 'Alice'. A pessoa está usando uma pulseira de metal no pulso direito." class="img-fluid">
+        </div>
+      </div>
     </article>
 
     <!------------------------------------------------- Chamando Footer ------------------------------------------------->
--- fatecon_2023.php
+++ fatecon_2023.php
@@ -3,127 +3,71 @@
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
 <?php require "includes/head.php"; ?>
 
-<title>DESENHOS E COSPLAYS</title>
+<title>FATECON 2023</title>
 
 <body>
   <div class="container text-center">
     <!------------------------------------------------- Chamando Header ------------------------------------------------->
     <?php require "includes/header_nav.php"; ?>
     <article>
-      <h2>Desenhos:</h2>
+      <h2>Veja algumas fotos do evento:</h2>
 
 
-      <div class="row">
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+      <div class="row justify-content-center">
+
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_1.webp" width="300" height="300" alt="Dois homens sorrindo. O homem à esquerda tem cabelo grisalho, barba cheia, usa óculos e uma jaqueta preta com listras vermelhas. O homem à direita tem cabelo curto, barba escura, usa óculos e uma camiseta preta com a frase 'JÁ LEU UM GIBI HOJE?'. Ao fundo, pessoas e uma mesa com balões amarelos." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_2.webp" width="300" height="300" alt="Quadro com vários quadrinhos coloridos e em preto e branco dispostos em uma grade. No canto inferior direito, há um logotipo com o texto 'FATEC COM'. O quadro está em um local iluminado e o fundo está escuro." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_3.webp" width="300" height="300" alt="Pessoa com cabelo escuro e longo, vestindo uma jaqueta azul com mangas brancas, está atrás de uma mesa cheia de itens de quadrinhos e produtos relacionados. A mesa tem uma toalha roxa e vários quadrinhos e acessórios expostos. Ao fundo, há outras pessoas e estandes em um ambiente de feira." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-      </div>
-      <div class="row">
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_4.webp" width="300" height="300" alt="Um grupo grande de pessoas posando para uma foto em um espaço coberto e amplo. Eles estão de pé e sentados, muitos sorrindo e fazendo gestos de paz ou alegria com as mãos. O ambiente parece ser uma área de armazenamento ou galpão, com o teto metálico e paredes de tijolo. No fundo, há um cartaz que diz 'MANEKI'. As pessoas na foto vestem roupas casuais e coloridas, algumas usando acessórios como chapéus e lenços." class="img-fluid">
         </div>
       </div>
       <div class="row">
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_5.webp" width="300" height="300" alt="Pessoa vestida com uma fantasia de guerreiro, usando uma armadura preta e azul com ombreiras, máscara e capacete. Ele segura uma espada futurística com iluminação roxa na mão direita e está em posição de combate. O ambiente é uma área interna com parede de tijolos brancos e uma porta marrom ao fundo." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_6.webp" width="300" height="300" alt="Espaço coberto com cadeiras organizadas e pessoas ao fundo. No primeiro plano, há um banner verde e amarelo da Gibiteca Balão, promovendo cultura pop/nerd na periferia, com menções a cinema e séries, HQs e mangás, terror, sci-fi, fantasia, RPG, board e card games, videogames, Just Dance e K-pop." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_7.webp" width="300" height="300" alt="Duas pessoas sorrindo em um evento. A pessoa à esquerda está vestindo uma jaqueta preta e uma camiseta preta, tem barba e cabelo grisalho. A pessoa à direita está vestindo uma camiseta preta com a inscrição 'JÁ LEU UM GIBI HOJE?' em rosa e branco, tem barba e cabelo escuro. Ao fundo, outras pessoas estão participando do evento, que parece ser em um espaço coberto." class="img-fluid">
         </div>
 
-        <div class="col-lg-3 col-md-4 col-sm-6">
-          <img src="https://placehold.co/300x300/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_8.webp" width="300" height="300" alt="Pessoa sorridente em um evento, vestindo uma camiseta preta com a inscrição 'JÁ LEU UM GIBI HOJE?' em rosa e branco. Ele está atrás de uma mesa cheia de quadrinhos e livros. Ao fundo, outras pessoas participam do evento em um espaço coberto, com balões amarelos e banners visíveis." class="img-fluid">
         </div>
-      </div>
-
-      <p>Lorem ipsum dolor sit amet consectetur. Vivamus nibh aliquam eget fames. Viverra sed lorem viverra sapien
-        tortor ornare. At fermentum non pellentesque morbi nisi egestas dictumst euismod. Ut vitae rhoncus est blandit
-        lorem aliquam. Fusce volutpat vel elementum duis a nisl sit mauris mauris. Eget donec dolor tincidunt imperdiet
-        pellentesque. Viverra in montes id id pharetra quam commodo quam metus. Cursus aenean fringilla facilisis
-        consequat nunc dolor lacinia potenti aliquet. Fermentum quisque lacus lacus augue. Lorem ipsum dolor sit amet
-        consectetur. Vivamus nibh aliquam eget fames. Viverra sed lorem viverra sapien tortor ornare. At fermentum non
-        pellentesque morbi nisi egestas dictumst euismod. Ut vitae rhoncus est blandit lorem aliquam. Fusce volutpat vel
-        elementum duis a nisl sit mauris mauris. Eget donec dolor tincidunt imperdiet pellentesque. Viverra in montes id
-        id pharetra quam commodo.<br><br><br> </p>
-
-
-
-
-
-      <h2>Cosplays:</h2>
-
-
-
-
-      <div class="row">
-
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img src="https://placehold.co/400x400/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img src="https://placehold.co/400x400/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img src="https://placehold.co/400x400/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
-        </div>
-
       </div>
       <div class="row">
 
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img src="https://placehold.co/400x400/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_9.webp" width="300" height="300" alt="Pessoa sentada em um evento, vestindo uma camiseta cinza com o logo da Marvel. Ela está atrás de uma mesa com quadrinhos e livros. Ao fundo, outras pessoas participam do evento em um espaço coberto." class="img-fluid">
         </div>
 
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img src="https://placehold.co/400x400/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_10.webp" width="300" height="300" alt="Um homem de óculos escuros e barba está em pé atrás de uma mesa cheia de quadrinhos. Ele veste uma jaqueta de couro preta e sorri. Na mesa há diversos quadrinhos coloridos expostos, incluindo títulos como 'Ado', 'Canil', 'Codinome', e 'Honório'. Outras pessoas estão ao fundo, participando de um evento em um local fechado." class="img-fluid">
         </div>
 
-        <div class="col-lg-4 col-md-4 col-sm-12">
-          <img src="https://placehold.co/400x400/000000/FFF" alt="Imagem com Placeholder" class="img-fluid">
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_11.webp" width="300" height="300" alt="Uma mulher de cabelo vermelho acaricia um grande cão preto e marrom que está deitado no chão. Ela usa um casaco rosa e calças estampadas. Outra pessoa de jeans está segurando a coleira do cão. A cena acontece em um espaço fechado com outras pessoas ao fundo." class="img-fluid">
         </div>
 
+        <div class="col-lg-3 col-md-3 col-sm-6 mt-3">
+          <img src="imagens/Fatec_2023_12.webp" width="300" height="300" alt="Uma pessoa vestida com uma fantasia de motosserra posando para uma foto com duas crianças. A pessoa fantasiada está com uma cabeça de motosserra e lâminas de motosserra nos braços, enquanto as crianças sorriem e posam ao lado. A cena acontece em um espaço fechado com mesas e cadeiras ao fundo." class="img-fluid">
+        </div>
       </div>
-
-      <p>Lorem ipsum dolor sit amet consectetur. Vivamus nibh aliquam eget fames. Viverra sed lorem viverra sapien
-        tortor ornare. At fermentum non pellentesque morbi nisi egestas dictumst euismod. Ut vitae rhoncus est blandit
-        lorem aliquam. Fusce volutpat vel elementum duis a nisl sit mauris mauris. Eget donec dolor tincidunt imperdiet
-        pellentesque. Viverra in montes id id pharetra quam commodo quam metus. Cursus aenean fringilla facilisis
-        consequat nunc dolor lacinia potenti aliquet. Fermentum quisque lacus lacus augue.<br><br><br> </p>
-
     </article>
     <!------------------------------------------------- Chamando Footer ------------------------------------------------->
     <?php require "includes/footer.php"; ?>

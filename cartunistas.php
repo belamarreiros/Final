8aeab7b1fcd4494b229ac71fc212868b049bfae3
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-06-21 22:38:39 -0300

    Alterando a cor da fonte dos botoes

--- cartunistas.php
+++ cartunistas.php
@@ -8,11 +8,11 @@
     <div class="container text-center">
         <!------------------------------------------------- Chamando Header ------------------------------------------------->
         <?php require "includes/header_nav.php"; ?>
-        <article class="mt-3">
+        <article class="mt-5">
             <hr>
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mt-5">
                 <h1>CARTUNISTAS</h1>
-                <p>
+                <p class="mt-4">
                     Um cartunista, também conhecido como cartoonista ou humorista gráfico, é um artista visionário que
                     se especializa em cartum, uma forma de arte que combina humor e crítica para retratar aspectos do
                     dia a dia de uma sociedade. Este trabalho é muitas vezes criado para entretenimento, comentário
@@ -31,16 +31,16 @@
                     Conheça alguns dos cartunistas que estavam presentes nas Fatecons:
                 </p>
             </div>
-            <hr>
-            <div>
+            <hr class="mt-4">
+            <div class="mt-5">
                 <figure class="banner_cartunistas">
                     <a href="https://www.instagram.com/stories/highlights/18204866779216593/" target="_blank">
                         <img src="imagens/Banner_cartunistas_01.webp" width="1296" height="106" alt="banner" class="img-banner" alt="Uma sequência de quadrinhos. Primeira imagem: uma lata. Segunda: um pedaço de fio enrolado. Terceira: a lata conectada ao fio. Quarta: a outra ponta do fio conectada a uma lata. Quinta: um personagem com um telefone de lata, dizendo 'Alô, é da FateCon?'. Sexta: outro personagem responde 'Isso mesmo! Estamos preparando tudo!'. Sétima: o mesmo personagem diz 'Legal demais que você vai vir pra São Roque, Capirotinho!'. Oitava: outro personagem aparece confuso e pergunta 'Ué... como assim Capirotinho?'. Nona: um personagem de óculos e orelhas grandes diz 'Eu sou o Rubens!'. Décima: uma mulher diz 'É nada?!'. Décima primeira: um homem de barba responde 'O Rubens é ele'. Décima segunda: a mulher pergunta 'E aí?'." />
                     </a>
                 </figure>
             </div>
-            <div class="row justify-content-center">
-                <div class="row col-lg-6 col-md-6 col-sm-12">
+            <div class="row justify-content-center mt-5">
+                <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                     <div class="col-lg-6 col-md-12 col-sm-12">
                         <img src="imagens/rapha.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras do cartunista Raphael Pinheiro. No canto inferior direito, em destaque, está a foto do cartunista, homem branco de cabelos escuto, usando oculos, magro, barba tem feita, camiseta marrom e um fundo branco.
                         " class="img_cartunista" />
@@ -55,7 +55,7 @@
                         <button onclick="window.location.href='rapha.php'">Veja mais</button>
                     </div>
                 </div>
-                <div class="row col-lg-6 col-md-6 col-sm-12">
+                <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                     <div class=" col-lg-6 col-md-12 col-sm-12">
                         <img src="imagens/natalia.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras da cartunista Natalia Sierpinski. No canto inferior direito, em destaque, está a foto da cartunista, branca, de oculos, sorrindo, cabelos pretos e camiseta rosa com um fundo branco " class="img_cartunista" />
                     </div>
@@ -70,14 +70,14 @@
                     </div>
                 </div>
             </div>
-            <hr>
-            <figure class="banner_cartunistas">
+            <hr class="mt-4">
+            <figure class="banner_cartunistas mt-5">
                 <a href="https://www.instagram.com/stories/highlights/18204866779216593/" target="_blank">
                     <img src="imagens/Banner_cartunistas_02.webp" width="1296" height="106" alt="banner" class="img-banner" alt="Uma sequência de quadrinhos. Primeira imagem: um personagem azul pega um telefone. Segunda: fala 'Deixa eu brincar também?'. Terceira: atende o telefone, pergunta se é o 'Capirotinho?'. Quarta: cena muda para um homem atendendo o telefone, pergunta 'Alô? Cecília? O quê?'. Quinta: o homem se assusta com o telefone explodindo. Sexta: ele observa o telefone danificado. Sétima: outro homem atende um telefone antigo e fala 'Alô Federico? Meu celular tijolão dos anos 80 explodiu. Tô falando num daqueles telefones de filme antigo. Oi? Tá me ouvindo?'. Oitava: uma mulher trabalha em uma torre, escuta um telefone tocar. Nona: ela diz 'Ei, Charlie! Consegue atender na cabine?'. Décima: Charlie responde 'Tô indo!'. Décima primeira: a mulher atende o telefone e diz 'Alô? Oi, Bruno! O Federico... sei... nossa! Explodiu?'." />
                 </a>
             </figure>
             <div class="row mt-3 justify-content-center">
-                <div class="row col-lg-6 col-md-6 col-sm-12">
+                <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                     <div class="col-lg-6 col-md-12 col-sm-12">
                         <img src="imagens/daniel.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras do cartunista Daniel Esteves. No canto inferior direito, em destaque, está a foto do cartunista, branco, sorrindo, cabelos e barba acizentados, aparentemente sendo entrevistado com o fundo sendo um telão." class="img_cartunista" />
                     </div>
@@ -91,7 +91,7 @@
                         <button onclick="window.location.href='daniel.php'">Veja mais</button>
                     </div>
                 </div>
-                <div class="row col-lg-6 col-md-6 col-sm-12">
+                <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                     <div class="col-lg-6 col-md-12 col-sm-12">
                         <img src="imagens/jefferson.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras do cartunista Jefferson Costa. No canto inferior direito, em destaque, está a foto do cartunista, moreno, cabelo escuro, vestindo uma blusa preta, barba bem feita, com um fundo branco. " class="img_cartunista" />
                     </div>
@@ -106,13 +106,13 @@
                     </div>
                 </div>
                 <hr class="mt-4">
-                <figure class="banner_cartunistas">
+                <figure class="banner_cartunistas mt-5">
                     <a href="https://www.instagram.com/stories/highlights/18204866779216593/" target="_blank">
                         <img src="imagens/Banner_cartunistas_03.webp" width="1296" height="106" alt="banner" class="img-banner" alt="Uma sequência de tirinhas de cartunistas promovendo a FateCon. A primeira tirinha mostra um personagem escalando uma parede e dizendo '12 de maio também pousaremos na FateCon'. A segunda tirinha mostra um personagem ao telefone dizendo 'Vou ligar pro nosso amigo o Ezim!'. A terceira e quarta tirinhas mostram personagens discutindo sobre a FateCon e um robô perguntando sobre o evento. As próximas tirinhas mostram um polvo roxo dizendo 'Alô? FateCon?', seguido de um coelho oferecendo o telefone a outro personagem. As últimas tirinhas mostram uma garota recebendo ajuda para falar com um representante da FateCon, que confirma o evento nos dias 12 e 13 de maio." />
                     </a>
                 </figure>
                 <div class="row mt-3 justify-content-center">
-                    <div class="row col-lg-6 col-md-6 col-sm-12">
+                    <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                         <div class="col-lg-6 col-md-12 col-sm-12">
                             <img src="imagens/gilmar.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras do cartunista Gilmar o cartunista das cavernas. No canto inferior direito, em destaque, está a foto do cartunista, branco, usando oculos, barba branca, cabelo preto, camiseta preta, segurando a imagem de um desenho em um fundo repleto de desenhos. " class="img_cartunista" />
                         </div>
@@ -126,7 +126,7 @@
                             <button onclick="window.location.href='gilmar.php'">Veja mais</button>
                         </div>
                     </div>
-                    <div class="row col-lg-6 col-md-6 col-sm-12">
+                    <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                         <div class="col-lg-6 col-md-12 col-sm-12">
                             <img src="imagens/guilherme.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras do cartunista Guilherme infante. No canto inferior direito, em destaque, está a foto do cartunista, branco, de cabelos escuros, barba bem feita, utilisando uma camisa florida e um bone vermelho, ao seu fundo percebos existem figuras que indicam que está sendo entrevistado " class="img_cartunista" />
                         </div>
@@ -141,13 +141,13 @@
                         </div>
                     </div>
                     <hr class="mt-4">
-                    <figure class="banner_cartunistas">
+                    <figure class="banner_cartunistas mt-5">
                         <a href="https://www.instagram.com/stories/highlights/18204866779216593/" target="_blank">
                             <img src="imagens/Banner_cartunistas_04.webp" width="1296" height="106" alt="banner" class="img-banner" alt="Uma sequência de tirinhas de cartunistas promovendo a FateCon. A primeira tirinha mostra um cachorro de óculos com um microfone, dizendo 'Nós estaremos na FateCon nos dias 12 e 13 de maio'. A segunda tirinha mostra o mesmo cachorro dizendo 'Cuidaremos de você para cuidar da mesa'. A terceira e quarta tirinhas mostram um personagem com óculos grandes e uma caneta, dizendo 'Oi, Brisa! Eu também estarei na FateCon!'. As últimas tirinhas mostram um personagem em uma caverna, dizendo 'Estarei ao lado de muita gente bacana dos quadrinhos' e 'Na FateCon, em São Roque nos dias 12 e 13 de maio! CONTINUA...'." />
                         </a>
                     </figure>
                     <div class="row mt-3 justify-content-center">
-                        <div class="row col-lg-6 col-md-6 col-sm-12">
+                        <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                             <div class="col-lg-6 col-md-12 col-sm-12">
                                 <img src="imagens/cecilia.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras da cartunista Cecilia Martins. No canto inferior direito, em destaque, está a foto da cartunista, branca, de oculos, cabelo preto, sorrindo, apoiando o rosto com a mão direita enquanto está sentada em uma cadeira de estofamento vermelho e debruçada sobre uma mesa branca, vestida com uma roupa escura com detalhes marelos, seu fundo percemos uma lampada e um fundo roxo." class="img_cartunista" />
                             </div>
@@ -162,7 +162,7 @@
 
                             </div>
                         </div>
-                        <div class="row col-lg-6 col-md-6 col-sm-12">
+                        <div class="row col-lg-6 col-md-6 col-sm-12 mt-5">
                             <div class="col-lg-6 col-md-12 col-sm-12">
                                 <img src="imagens/marcel.webp" width="300" height="300" alt="Mascote da FateconHQs! com o nome do evento 'FateconHQs!' abaixo dele. No centro da imagem, há fotos das obras do cartunista Marcel Bartholo. No canto inferior direito, em destaque, está a foto do cartunista, branco, de oculos arredondados e escuros, barba alta por fazer, sorrindo, com um fundo branco onde se percebe alguns desenhos nele." class="img_cartunista" />
                             </div>
@@ -176,8 +176,8 @@
                                 </p>
                             </div>
                         </div>
-                        <hr>
-                        <div class="row mt-3 justify-content-center">
+                        <hr class="mt-4">
+                        <div class="row mt-3 justify-content-center mt-4">
                             <p>
                                 Historicamente, os cartunistas sempre foram um meio poderoso de expressão do sentimento
                                 popular. No Brasil, o cartum desempenhou um papel crucial durante os movimentos contra a

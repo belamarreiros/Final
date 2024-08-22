73a4013145f11d58da2f63d3a195c5136131b48e
Izabela Fernanda <izabelamarreiros@outlook.com>
2024-08-22 20:25:54 -0300

    Alterei o PT-BR

--- cecilia.php
+++ cecilia.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="PT-BR">
 <!------------------------------------------------- Chamando Head ------------------------------------------------->
 <?php require "includes/head.php"; ?>
 <title>CECILIA MARTINS</title>
